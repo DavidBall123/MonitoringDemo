396938a7cbd8d109b872f862886728b7ff562e15
David Ball <david.ball@agriculture.gov.au>
2021-09-24 08:05:10 +1000

    initial check in

---
+++ Service/ExampleService/Models/WeatherExample.cs
@@ -0,0 +1,12 @@
+﻿namespace ExampleService.Models
+{
+    public class WeatherExample
+    {
+		public int Id { get; set; }
+	    public string City { get; set; }
+
+	    public int High { get; set; }
+
+	    public int Low { get; set; }
+    }
+}
